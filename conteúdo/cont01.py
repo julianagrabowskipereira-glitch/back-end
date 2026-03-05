754404755892874035571d2b49a7e24e0eb67774
julianagrabowskipereira-glitch <juliana.grabowski.pereira@escola.pr.gov.br>
2026-03-05 11:46:02 -0300

    Create cont01.py

---
+++ conteúdo/cont01.py
@@ -0,0 +1,4 @@
+#Int= para numeros inteiros. Ex: 7, 0, -4
+#Float= para números reais ou com virgula. Ex: 0.45, 7.6
+#Bool= para valores lógicos de verdadeiro ou falso. Ex True ou False( sempre usar com letra maiúscula
+#Str= para strings que são palavras caracteres ou textos. Ex: 'ola', '7.5'
