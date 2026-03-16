88cddb6b2287df4cd606f9574662e815bd18cb63
julianagrabowskipereira-glitch <juliana.grabowski.pereira@escola.pr.gov.br>
2026-03-16 11:01:30 -0300

    Update cont06.py

--- conteúdo/cont06.py
+++ conteúdo/cont06.py
@@ -7,3 +7,18 @@ print('prazer em conhece-lo {:20}!'.format(nome))
 Qual é o seu nome? Gustavo
 
 prazer em conhece-lo Gustavo
+#LINHAMENTO DE MASCARA
+
+#Podemos alinhar o nome dentro do número de caracteres que desejamos
+
+A#linhamento para direita
+nome = input ('Qual é o seu nome?')
+print('prazer em conhece-lo {:>20}!'.format(nome))
+
+#Alinhamento para a esquerda
+nome = input ('Qual é o seu nome?')
+print('prazer em conhece-lo {:<20}!'.format(nome))
+
+#Alinhamento no centro
+nome = input ('Qual é o seu nome?')
+print('prazer em conhece-lo {:^20}!'.format(nome))
