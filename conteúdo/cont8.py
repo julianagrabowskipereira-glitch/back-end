19a3ae4b844bfc955a54822f92c2c3c2e9f04c3f
julianagrabowskipereira-glitch <juliana.grabowski.pereira@escola.pr.gov.br>
2026-03-26 08:51:18 -0300

    Create cont8.py

---
+++ conteúdo/cont8.py
@@ -0,0 +1,6 @@
+importando funcionalidades específicas
+from math import sqrt, floor, ceil
+num = int(input('Digite um número: ' ))
+raiz = sqrt(num)
+print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz))) arredonda pra cima
+print('a raiz de {} é igual a {}'.format(num, floor(raiz)))
