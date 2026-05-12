15eb54bd6dd706794a79dd14c6d276fadba27ff1
julianagrabowskipereira-glitch <juliana.grabowski.pereira@escola.pr.gov.br>
2026-05-12 09:30:56 -0300

    Create cont12.py with conditional statements explanation
    Add a Python script explaining conditional statements with examples.

---
+++ conteúdo/cont12.py
@@ -0,0 +1,29 @@
+Condições
+Estrutura sequencial: Os comandos condicionais são executados de cima para baixo em um bloco de códigos. 
+Até esse momento estávamos trabalhando nessa lógica, porém nas estruturas condicionais temos duas possibilidades de caminhos. 
+Cada caminho terá um bloco de comando executados se ele for escolhido. 
+Em programação utilizamos o comando se, se alguma condição verdadeira executa um bloco de códigos, e podemos utilizar um bloco se não para executar quando a condição for falsa.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+                                                                                                                                                                                                                                                                               Condições
+Em condicionais devemos deixar o código indentado, ou seja, com o espaçamento correto demonstrando qual bloco de código pertece ao operador condicional. 
+Para realizar a indentração podemos utilizar a tecla tab. 
+Em python para o se utilizamos if+confição+:, já o senão utilizamos o else + :, depois dos dois pontos nos dois casos colocamos os blocos de códigos que serão executados. 
+Nesse tipo de condicional apenas um bloco é executado.
+
+
+Condições (exemplo)
+tempo = int(input("Quantos anos tem seu carro?"))
+If tempo<=3:
+print('carro novo')
+else:
+print('carro velho)
+Print('--fim-')
+
+exemplo
+home = str(input('Qual é o seu nome?
+if nome == 'Gustavo':
+print('Que nome lindo você tem!')
+else:
+print('seu nome é tão normal')
+print('Bom dia {}!'.format(nome))
+      
